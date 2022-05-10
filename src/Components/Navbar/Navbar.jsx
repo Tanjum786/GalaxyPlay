22c8d6ccb4d05e05668564b689c0277150f0f1b4
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-05-10 09:47:02 +0530

    feat : added play icon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,7 +11,12 @@ import { FaUserCircle } from "react-icons/fa";
 
 export const Navbar = () => {
   const [open, setopen] = useState(false);
-  const { userDetailes, setuserDetailes, authState:{isSubmit},Authdispatch } = useAuth();
+  const {
+    userDetailes,
+    setuserDetailes,
+    authState: { isSubmit },
+    Authdispatch,
+  } = useAuth();
   const { token, user } = userDetailes;
   const navigate = useNavigate();
   const location = useLocation();
@@ -26,7 +31,7 @@ export const Navbar = () => {
     localStorage.removeItem("userEncodedToken");
     localStorage.removeItem("users");
     setuserDetailes({ token: "", user: {} });
-    Authdispatch({type:"LOGOUT"})
+    Authdispatch({ type: "LOGOUT" });
     navigate("/");
     toast.success("Logout successfully");
   };
@@ -49,9 +54,12 @@ export const Navbar = () => {
               : hamburgIcon}
             {open && <HamburgerMenu />}
             <NavLink to="/">
-              <h1 className="app-titel">
-                Galaxy<span className="span-color">Play</span>
-              </h1>
+              <div className="dis_flex title-containe">
+                <h1 className="app-titel">
+                  Galaxy<span className="span-color">Play</span>
+                </h1>
+                <i class="fa-solid fa-play play-icon"></i>
+              </div>
             </NavLink>
           </div>
           {location.pathname === "/login" ||
@@ -76,10 +84,9 @@ export const Navbar = () => {
                   <span className="user-name"> {user.firstName}</span>
                 </div>
                 <div>
-                  
-                <button className="login_btn" onClick={logoutHandler}>
-                  Logout
-                </button>
+                  <button className="login_btn" onClick={logoutHandler}>
+                    Logout
+                  </button>
                 </div>
               </>
             ) : (
