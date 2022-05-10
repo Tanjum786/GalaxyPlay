07582ee6c2f34036201e957bbb2350c9290afc0d
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-05-10 15:06:30 +0530

    bug: correct the typo

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,20 +10,20 @@ export const Footer = () => {
         </div>
         <ul className="social_media_links dis_flex gap_s">
           <li className="list-item-inline">
-            <a href="https://github.com/Tanjum786" target="blank_">
+            <a href="https://github.com/Tanjum786" target="_blank">
               <i className="icon_footer padding_small fab fa-github"></i>
             </a>
           </li>
           <li className="list-item-inline">
             <a
               href="https://www.linkedin.com/in/tanjum-kadakol-665a69225/"
-              target="blank_"
+              target="_blank"
             >
               <i className="icon_footer padding_small fab fa-linkedin"></i>
             </a>
           </li>
           <li className="list-item-inline">
-            <a href="https://twitter.com/KadakolTanjum" target="blank_">
+            <a href="https://twitter.com/KadakolTanjum" target="_blank">
               <i className="icon_footer padding_small fa-brands fa-twitter-square fotter_icon"></i>
             </a>
           </li>
