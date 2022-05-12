5d6947fe78ce0dc5b11d60db7a6d3f875458faa7
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-05-12 15:55:35 +0530

    feat : add chips to video listing page

---
+++ src/Pages/VideoListing/Chips/Chips.jsx
@@ -0,0 +1,15 @@
+import "./chips.css";
+export const Chips = () => {
+  return (
+      <div className="chips-container">
+        <div className="chips-list dis_flex">
+          <button className="btn-singer-name">All</button>
+          <button className="btn-singer-name">Arjit Singh</button>
+          <button className="btn-singer-name">Shreya Ghoshal</button>
+          <button className="btn-singer-name">Javed Ali</button>
+          <button className="btn-singer-name">Jubin Nautiyal</button>
+          <button className="btn-singer-name">Atif Aslam</button>
+        </div>
+      </div>
+  );
+};
