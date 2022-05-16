994dac55ae676eacfc82474c728931be3cae03bb
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-05-16 07:36:53 +0530

    feat  : barrel e export

--- src/ApiCalls/index.jsx
+++ src/ApiCalls/index.jsx
@@ -1,5 +1,6 @@
 import { addtoLike } from "./LikeVideoApi/addtoLike";
 import { deletelikeVideo } from "./LikeVideoApi/deletelikeVideo";
 import { getlikevideo } from "./LikeVideoApi/getlikevideo";
+import { deleteWatchlater } from "./WatchlaterApi/deleteWatchlater";
 
-export{addtoLike,deletelikeVideo,getlikevideo}
+export{addtoLike,deletelikeVideo,getlikevideo,deleteWatchlater}
