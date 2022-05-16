b5bbb2cce3fcecea2fdf1fe99b8dce2a1cc5b877
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-05-16 07:42:27 +0530

    feat : barrel export

---
+++ src/context/index.jsx
@@ -0,0 +1,17 @@
+import { AuthProvider } from "./auth-context/AuthContext";
+import { LikevideocontextProvider } from "./Liked-context/Likevideocontext";
+import { ShowpasswordProvider } from "./showpassword-context/ShowpasswordContext";
+import { Watchlatercontextprovider } from "./Watchlater-context/Watchlatercontext";
+import { useAuth } from "./auth-context/AuthContext";
+import { useLikeVideoContext } from "./Liked-context/Likevideocontext";
+import { usewatchlater } from "./Watchlater-context/Watchlatercontext";
+
+export {
+  AuthProvider,
+  LikevideocontextProvider,
+  ShowpasswordProvider,
+  Watchlatercontextprovider,
+  useAuth,
+  useLikeVideoContext,
+  usewatchlater
+};
