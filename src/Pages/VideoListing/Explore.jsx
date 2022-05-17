312778fc566f49055fa5cf17e8f64ce10d52bd89
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-05-17 17:36:21 +0530

    refactor : removed component of chip added in explore

--- src/Pages/VideoListing/Explore.jsx
+++ src/Pages/VideoListing/Explore.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { fetchData, getcategorydata } from "../../ApiCalls";
 import { Footer, Navbar, SideBar, VideoCard } from "../../Components";
