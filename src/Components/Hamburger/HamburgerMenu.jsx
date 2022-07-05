96c2de395f1faf8dc4b8c25390478617d2ee3de5
Tanjum786 <84380051+Tanjum786@users.noreply.github.com>
2022-07-05 11:04:34 +0530

    refactor : fixed hamburger to mobile responsive

--- src/Components/Hamburger/HamburgerMenu.jsx
+++ src/Components/Hamburger/HamburgerMenu.jsx
@@ -3,53 +3,83 @@ import { NavLink } from "react-router-dom";
 import { useAuth } from "../../context/auth-context/AuthContext";
 import "./hamburger.css";
 export const HamburgerMenu = () => {
-  const {userDetailes}=useAuth();
-  const {token,user}=userDetailes;
+  const { userDetailes } = useAuth();
+  const { token, user } = userDetailes;
   return (
     <>
       <aside>
         <ul className="hamburger-sidebar-container">
           <li className="hamburger-sidebar-list">
-            <NavLink to="/" className={({isActive})=>(isActive?"activeLink":"notactive")}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "activeLink" : "notactive"
+              }
+            >
               <i className="icon-hamburger-sidebar fa-solid fa-compass"> </i>
               <span className="title-hamburger-sidebar">Explore</span>
             </NavLink>
           </li>
           <li className="hamburger-sidebar-list">
-            <NavLink to="/likevideo" className={({isActive})=>(isActive?"activeLink":"notactive")}>
+            <NavLink
+              to="/likevideo"
+              className={({ isActive }) =>
+                isActive ? "activeLink" : "notactive"
+              }
+            >
               <i className="icon-hamburger-sidebar fa-solid fa-thumbs-up "></i>
               <span className="title-hamburger-sidebar">Liked</span>
             </NavLink>
           </li>
           <li className="hamburger-sidebar-list">
-            <NavLink to="/playlist" className={({isActive})=>(isActive?"activeLink":"notactive")}>
+            <NavLink
+              to="/playlist"
+              className={({ isActive }) =>
+                isActive ? "activeLink" : "notactive"
+              }
+            >
               <i className="icon-hamburger-sidebar fa-solid fa-folder-plus"></i>
               <span className="title-hamburger-sidebar">playlist</span>
             </NavLink>
           </li>
           <li className="hamburger-sidebar-list">
-            <NavLink to="/wacthlater" className={({isActive})=>(isActive?"activeLink":"notactive")} >
+            <NavLink
+              to="/wacthlater"
+              className={({ isActive }) =>
+                isActive ? "activeLink" : "notactive"
+              }
+            >
               <i className="icon-hamburger-sidebar fa-solid fa-clock"></i>
               <span className="title-hamburger-sidebar">WatchLater</span>
             </NavLink>
           </li>
           <li className="hamburger-sidebar-list">
-            <NavLink to="/history" className={({isActive})=>(isActive?"activeLink":"notactive")} >
+            <NavLink
+              to="/history"
+              className={({ isActive }) =>
+                isActive ? "activeLink" : "notactive"
+              }
+            >
               <i className="icon-hamburger-sidebar fa-solid fa-clock-rotate-left"></i>
               <span className="title-hamburger-sidebar">History</span>
             </NavLink>
           </li>
           {token && user ? (
-            
-            <li className="hamburger-sidebar-list">
-              <i className="icon-hamburger-sidebar fa-solid fa-circle-user"></i>
-              <span className="title-hamburger-sidebar">{user.firstName}</span>
-            </li>
+            <NavLink to="/profilepage">
+              <li className="hamburger-sidebar-list">
+                <i className="icon-hamburger-sidebar fa-solid fa-circle-user"></i>
+                <span className="title-hamburger-sidebar">
+                  {user.firstName}
+                </span>
+              </li>
+            </NavLink>
           ) : (
-            <li className="hamburger-sidebar-list">
-              <i className="icon-hamburger-sidebar fa-solid fa-circle-user"></i>
-              <span className="title-hamburger-sidebar">Profile</span>
-            </li>
+            <NavLink to="/login">
+              <li className="hamburger-sidebar-list">
+                <i className="icon-hamburger-sidebar fa-solid fa-circle-user"></i>
+                <span className="title-hamburger-sidebar">Profile</span>
+              </li>
+            </NavLink>
           )}
         </ul>
       </aside>
